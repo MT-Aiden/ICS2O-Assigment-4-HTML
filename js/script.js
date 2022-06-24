87957d11ef829fbf6085bc7d588febe292fdcb5a
MT-Aiden <99355180+MT-Aiden@users.noreply.github.com>
2022-06-24 02:26:04 +0000

    work

--- js/script.js
+++ js/script.js
@@ -1,18 +1,48 @@
-//Made by: Aiden McLeod
-//Made in: March 2022
+// Aiden McLeod
+// ICS2O-Assignment4-HTML
+// April 19 2022
 
 'use strict'
 /**
- * This function calculates area of a sphere.
+ * Check service worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Assignment4-HTML/sw.js", {
+    scope: "/ICS2O-Assignment4-HTML/",
+  })
+}
+
+/**
+ * Calculates the price of the pizza the user orders.
  */
 function calculate() {
-  // input
-  const radius = parseInt(document.getElementById('radius').value)
+  // Box booleans
+  const large = document.getElementById("large").checked
+  const extralarge = document.getElementById("extra-large").checked
+  const onetopping = document.getElementById("one-topping").checked
+  const twotoppings = document.getElementById("two-toppings").checked
+  const threetoppings = document.getElementById("three-toppings").checked
+  const fourtoppings = document.getElementById("four-toppings").checked
+  
+  // Price varibles
+  var sizeprice
+  var toppingprice
+  
+  if (large == true) {
+    sizeprice = 6
+  } else if (extralarge == true) {
+    sizeprice = 10
+  }
 
-  // process
-
-  const volume = (4/3)* Math.PI * Math.pow(radius, 3);
-
-  // output
-  document.getElementById('volume').innerHTML = 'Volume is: ' + volume.toFixed(2) + ' cm³'
+  if (onetopping == true) {
+    toppingprice = 1
+  } else if (twotoppings == true) {
+    toppingprice = 1.75
+  } else if (threetoppings == true) {
+    toppingprice = 2.5
+  } else if (fourtoppings == true) {
+    toppingprice = 3.35
+  }
+  
+  document.getElementById("output").innerHTML = "<br><p>The cost of this pizza is $" + ((sizeprice + toppingprice) + ((sizeprice + toppingprice) * 0.13)).toFixed(2) + "</p>"
 }
